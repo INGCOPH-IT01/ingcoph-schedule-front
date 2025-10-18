9c6a84de865191e58fce82dd7f1722ba7fcc1699
Karlo Alfonso <alfonsokarlo@gmail.com>
2025-10-18 18:26:04 +0800

    enforce https

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), basicSsl()],
   server: {
     port: 3000,
+    https: true,
     host: true,
     proxy: {
       '/api': {
