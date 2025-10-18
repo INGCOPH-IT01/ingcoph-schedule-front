3c4919fe7c9264f61b8bac88e5149c5989eb61d1
Karlo Alfonso <alfonsokarlo@gmail.com>
2025-10-18 18:31:52 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,29 +2,34 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 
-export default defineConfig({
-  plugins: [vue(), basicSsl()],
-  server: {
-    port: 3000,
-    https: true,
-    host: true,
-    proxy: {
-      '/api': {
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'vue-vendor': ['vue', 'vue-router'],
-          'vuetify': ['vuetify'],
-          'utilities': ['axios', 'laravel-echo', 'pusher-js']
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [vue(), basicSsl()],
+    define: {
+      'process.env.NODE_ENV': JSON.stringify(mode === 'production' ? 'production' : 'development')
+    },
+    server: {
+      https: true,
+      port: 5555,
+      host: true,
+      proxy: {
+        '/api': {
+          changeOrigin: true,
+          secure: false,
         }
       }
     },
-    chunkSizeWarningLimit: 1000
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            'vue-vendor': ['vue', 'vue-router'],
+            'vuetify': ['vuetify'],
+            'utilities': ['axios', 'laravel-echo', 'pusher-js']
+          }
+        }
+      },
+      chunkSizeWarningLimit: 1000
+    }
   }
 })
